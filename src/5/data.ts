405bdda8a8718db2a8c2b583a4db25eaa4ee0e8e
Peter Osama <peter.osama@vezeeta.com>
2023-07-12 00:59:48 +0300

    add more items

--- src/5/data.ts
+++ src/5/data.ts
@@ -2,10 +2,13 @@ export const Items = [
   { key: 1, value: "Item1" },
   { key: 2, value: "Item2" },
   { key: 3, value: "Item3" },
+  { key: 32, value: "Item32" },
+  { key: 33, value: "Item33" },
   { key: 4, value: "Item4" },
   { key: 5, value: "Item5" },
   { key: 6, value: "Item6" },
   { key: 7, value: "Item7" },
+  { key: 77, value: "Item77" },
 ];
 
 export interface itemInterface {
